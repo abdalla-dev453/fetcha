0bcdbfcbc3821aa405474e854d46f106a93c409b
abdalla-dev453 <abdallamsema69@gmail.com>
2026-06-03 15:39:45 +0300

    chore: initialize a fetcha project

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@
 const Home = () => {
   return (
     <div>
-        <h1>WElcome to Fetcha</h1>
+        <h1>Welcome to Fetcha</h1>
         <p>
             Order food from your favorite restaurants.
         </p>
