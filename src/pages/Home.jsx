a106485de08fc7d7095a9d4701cb2996e026dc7f
abdalla-dev453 <abdallamsema69@gmail.com>
2026-06-03 15:37:49 +0300

    chore: initialize fetcha project

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+
+const Home = () => {
+  return (
+    <div>
+        <h1>WElcome to Fetcha</h1>
+        <p>
+            Order food from your favorite restaurants.
+        </p>
+    </div>
+  )
+}
+
+export default Home
